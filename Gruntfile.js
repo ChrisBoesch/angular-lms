0a5b0cca5f1fb0d6fd2a36ea797a9483bef44752
A. Prooks <aprooks@live.ru>
2014-03-13 18:48:16 +0300

    chore: default port set to 8888
    Signed-off-by: A. Prooks <aprooks@live.ru>

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function (grunt) {
 
     express: {
       options: {
-        port: process.env.PORT || 9000
+        port: process.env.PORT || 8888
       },
       dev: {
         options: {
